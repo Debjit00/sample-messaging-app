c27db2dd7a5ac4ac492048e354258494fb2503ab
Debjit00 <debjitsaha419@gmail.com>
2025-03-10 11:13:48 +0530

    Fix HTTPS configuration

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ const path = require('path');
 // Create Express app
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+// const io = socketIo(server);
+const io = socketIo(server, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+});
 
 // Set EJS as the view engine
 app.set('view engine', 'ejs');
@@ -15,10 +21,17 @@ app.set('views', path.join(__dirname, 'views'));
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+    if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
+      return res.redirect(`https://${req.headers.host}${req.url}`);
+    }
+    next();
+});
 // Routes
 app.get('/', (req, res) => {
   res.render('index');
 });
+
 
 // Socket.IO connection handling
 io.on('connection', (socket) => {
